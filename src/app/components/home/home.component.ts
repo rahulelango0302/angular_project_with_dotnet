4d97d366c4b402262d0c89fdc3c4dde94c2d144c
rahulelango0302 <rahulelango1999@gmail.com>
2025-01-07 22:23:40 +0530

    added plans and pricing section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { PlansAndPricingComponent } from "../plans-and-pricing/plans-and-pricing.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselModule],
+  imports: [CarouselModule, PlansAndPricingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
